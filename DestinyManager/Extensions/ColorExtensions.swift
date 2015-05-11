37afaec9be79d9a42d445859391836c153cb9033
Shady <shady@shadyproject.net>
2015-05-11 16:38:55 -0500

    fix compiler error

--- DestinyManager/Extensions/ColorExtensions.swift
+++ DestinyManager/Extensions/ColorExtensions.swift
@@ -10,8 +10,23 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    //TODO: make these static once it's possible
-    internal static let DestinyGreen = UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 0.0/255.0, alpha: 1)
-    internal static let DestinyOrange = UIColor(red: 255.0/255.0, green: 153.0/255.0, blue: 0.0/255.0, alpha: 1)
-    internal static let DestinyYellow = UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 0.0/255.0, alpha: 1)
+    //NOTE: ugly syntax required to avoid compiler error
+    internal static var DestinyGreen: UIColor  {
+        get {
+            return UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 0.0/255.0, alpha: 1)
+        }
+    }
+    
+    
+    internal static var DestinyOrange: UIColor {
+        get {
+            return UIColor(red: 255.0/255.0, green: 153.0/255.0, blue: 0.0/255.0, alpha: 1)
+        }
+    }
+    
+    internal static var DestinyYellow: UIColor {
+        get {
+            return UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 0.0/255.0, alpha: 1)
+        }
+    }
 }
