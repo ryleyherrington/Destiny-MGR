835138d9813dd6462cf590691dfdecac0e8314f7
Shady <shady@shadyproject.net>
2015-05-11 17:35:02 -0500

    Refactoring, checking for user from call
    We now have an actual validation function (but still bad UI) and we
    check to see if the PlayerInfo call actually gives us a player info.

--- DestinyManager/LoginViewController.swift
+++ DestinyManager/LoginViewController.swift
@@ -18,15 +18,12 @@ class LoginViewController: UIViewController {
     internal static let StoryboardIdentifier = "LoginViewController"
     
     @IBAction func loadCharacter(sender: UIButton) {
-        //todo move this into it's own validation func
-        //also this is pretty shit UX so don't do it
-        if characterNameField.text == "" {
-            let alert = UIAlertView(title: "Name", message: "Please enter a character name.", delegate: self, cancelButtonTitle: "Okay")
-            alert.show()
+        if !loginValid() {
             return
         }
         
         toggleLoadingUi(true)
+        
         let name = self.characterNameField.text
         let system = SystemType(rawValue: self.systemSelector.selectedSegmentIndex+1)!
         var newPlayer = PlayerInfo(name: name, system: system, hash: nil)
@@ -38,14 +35,34 @@ class LoginViewController: UIViewController {
         
         ServiceManager.fetchPlayerAsync(newPlayer, completion: { (info) -> Void in
             dispatch_async(dispatch_get_main_queue()){
-                //TODO: info should give success or failure or something like that
-                info.addToStorage()
                 
                 self.toggleLoadingUi(false)
-                self.dismissViewControllerAnimated(true, completion: nil)
+                
+                if info != nil {
+                    info!.addToStorage()
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                } else {
+                    let alert = UIAlertView(title: "Error:", message: "Could not load character with that information", delegate: self, cancelButtonTitle: "Try Again")
+                    alert.show()
+                }
             }
         })
+    }
+    
+    //TODO: this should be a fancy inline thing with colors and * and auto-focusing and such
+    func loginValid() -> Bool {
+        if characterNameField.text == "" {
+            let alert = UIAlertView(title: "Name", message: "Please enter a character name.", delegate: self, cancelButtonTitle: "Okay")
+            alert.show()
+            return false
+        }
         
+        if systemSelector.selectedSegmentIndex < 0 {
+            characterNameField.layer.borderColor = UIColor.redColor().CGColor
+            return false
+        }
+        
+        return true
     }
     
     func toggleLoadingUi(loading: Bool) -> Void {
